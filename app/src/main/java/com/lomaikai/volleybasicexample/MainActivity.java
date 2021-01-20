e1dca5cb68c646bc6943483286016bac1fbeb1d3
Ian Chai <ianchai@mmu.edu.my>
2021-01-20 18:29:43 +0800

    Working now.

--- app/src/main/java/com/lomaikai/volleybasicexample/MainActivity.java
+++ app/src/main/java/com/lomaikai/volleybasicexample/MainActivity.java
@@ -3,6 +3,7 @@ package com.lomaikai.volleybasicexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,27 +15,26 @@ import com.android.volley.toolbox.Volley;
 public class MainActivity extends AppCompatActivity {
     private RequestQueue mRequestQueue;
     private StringRequest mStringRequest;
-    private String url = getString(R.string.uri);
+    private String url = "https://www.gutenberg.org/files/59281/59281-0.txt";
+    private TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        output = findViewById(R.id.outputTextView);
+        output.setMovementMethod(new ScrollingMovementMethod());
     }
 
     public void sendAndRequestResponse(View view) {
-        //RequestQueue initialized
-        mRequestQueue = Volley.newRequestQueue(this);
-        //String Request initialized
-        mStringRequest = new StringRequest(Request.Method.GET,
+        mRequestQueue = Volley.newRequestQueue(this);  //mRequestQueue is a RequestQueue
+        mStringRequest = new StringRequest(Request.Method.GET, //mStringRequest is a StringRequest
                 url,
                 response -> {
-                    TextView t = findViewById(R.id.outputTextView);
-                    t.setText(response);
+                    output.setText(response);
                 },
                 error -> {
-                    TextView t = findViewById(R.id.outputTextView);
-                    t.setText(getString(R.string.errorNotice)+error);
+                    output.setText(getString(R.string.errorNotice)+error);
                 });
         mRequestQueue.add(mStringRequest);
     }
